d745e1313e97d55c4c6723a3159be974a9e520ac
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-25 23:16:23 -0300

    feature: notify function to notify other layers feature: ExecValidator generic function to execute validation on business layer

---
+++ src/App.BusinessLayer/Services/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using App.BusinessLayer.Entities;
+using App.BusinessLayer.Notifications;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BusinessLayer.Services
+{
+    public abstract class BaseService(INotifier notifier)
+    {
+        private readonly INotifier _notifier = notifier;   
+        protected void Notify(ValidationResult validationResult)
+        {
+            validationResult.Errors.ForEach(e =>
+            {
+                Notify(e.ErrorMessage);
+            });
+        }
+
+        protected void Notify(string message)
+        {
+            _notifier.Handle(message);
+        }
+
+        protected bool ExecValidator<TValidator, TEntity>(TValidator validator, TEntity entity) 
+            where TValidator : AbstractValidator<TEntity> 
+            where TEntity : Entity
+        {
+            var validatorResult = validator.Validate(entity);
+           
+            return validatorResult.IsValid;
+        }
+    }
+}
