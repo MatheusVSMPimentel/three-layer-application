6870ff4ba833f8d59d4f44ced5271edebb678f3d
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-25 23:24:47 -0300

    feature: product service

---
+++ src/App.BusinessLayer/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using App.BusinessLayer.Entities;
+using App.BusinessLayer.Entities.Validations;
+using App.BusinessLayer.Interfaces.Repositories;
+using App.BusinessLayer.Interfaces.Services;
+using App.BusinessLayer.Notifications;
+
+namespace App.BusinessLayer.Services
+{
+    public class ProductService(IProductRepository productRepository, INotifier notifier) : BaseService(notifier), IProductService
+    {
+        public async Task Add(Product product)
+        {
+            if (!ExecValidator(new ProductValidation(), product)) return;
+            await productRepository.Add(product);
+        }
+
+        public async Task Update(Product product)
+        {
+            await productRepository.Update(product);
+        }
+        public async Task Delete(Guid id)
+        {
+            await productRepository.Delete(id);
+        }
+
+        public void Dispose()
+        {
+            productRepository?.Dispose();
+        }
+    }
+}
