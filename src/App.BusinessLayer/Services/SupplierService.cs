06bdfa9d8a512807df18ee99a5f78648370dffbb
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-25 23:24:34 -0300

    feature: add supplier service with unique document constraint feature: update supplier service with document and supplier id match constraint feature: remove supplier service constrainted to remove a supplier only if he is without products and before remove address to don't give relationship exception

---
+++ src/App.BusinessLayer/Services/SupplierService.cs
@@ -0,0 +1,60 @@
+﻿using App.BusinessLayer.Entities;
+using App.BusinessLayer.Entities.Validations;
+using App.BusinessLayer.Interfaces.Repositories;
+using App.BusinessLayer.Interfaces.Services;
+using App.BusinessLayer.Notifications;
+
+namespace App.BusinessLayer.Services
+{
+    public class SupplierService(ISupplierRepository supplierRepository, INotifier notifier) : BaseService(notifier), ISupplierService
+    {
+        public async Task Add(Supplier supplier)
+        {
+            //TODO: Validation consistent entity?
+            //TODO: Validation Supplier has unique Document?
+            if (!ExecValidator(new SupplierValidation(), supplier) || !ExecValidator(new AddressValidation(), supplier?.Address)) return;
+            if (supplierRepository.SearchFunction(f => f.Document == supplier.Document).Result.Any())
+            {
+                Notify("Supplier Document already recorded in database.");
+                return;
+            }
+            await supplierRepository.Add(supplier);
+
+        }
+
+        public async Task Update(Supplier supplier)
+        {
+            if (supplierRepository.SearchFunction(f => f.Document == supplier.Document && f.Id == supplier.Id).Result.Any())
+            {
+                Notify("Supplier Document already recorded in database.");
+                return;
+            }
+            await supplierRepository.Update(supplier);
+        }
+        public async Task Delete(Guid id)
+        {
+            var supplier = await supplierRepository.GetSupllierProductsAndAdress(id);
+            if (supplier == null)
+            {
+                Notify("Supplier not existent in database records.");
+                return;
+            }
+            if (supplier?.Products?.Count > 0)
+            {
+                Notify("Supplier still have products related to him.");
+                return;
+            }
+            var address = await supplierRepository.GetSupplierAddress(id);
+            if (address != null)
+            {
+                await supplierRepository.RemoveSupplierAddress(address);
+            }
+            await supplierRepository.Delete(id);
+        }
+
+        public void Dispose()
+        {
+            supplierRepository?.Dispose();
+        }
+    }
+}
