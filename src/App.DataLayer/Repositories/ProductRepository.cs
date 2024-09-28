afa5394febf7d0855e2f1ad91834cd92cc6a5f4f
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-28 01:11:46 -0300

    feature: repository data layer implementation

---
+++ src/App.DataLayer/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using App.BusinessLayer.Entities;
+using App.BusinessLayer.Interfaces.Repositories;
+using App.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DataLayer.Repositories
+{
+    public class ProductRepository(MsSQLServerContext msSQLServerContext) : Repository<Product>(msSQLServerContext), IProductRepository
+    {
+        public async Task<IEnumerable<Product>> GetAllProductsSupplier()
+        {
+            return await DbSet.AsNoTracking().Include(p => p.Supplier).OrderBy(p => p.Name).ToListAsync();
+        }
+
+        public async Task<Product?> GetProductSupplierById(Guid productId)
+        {
+            return await DbSet.AsNoTracking().Include(p => p.Supplier).FirstOrDefaultAsync(p => p.Id.Equals(productId));
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsBySupplier(Guid supplierId)
+        {
+            return await SearchFunction(p => p.SupplierId.Equals(supplierId));
+        }
+    }
+}
---
+++ src/App.DataLayer/Repositories/SupplierRepository.cs
@@ -0,0 +1,31 @@
+﻿using App.BusinessLayer.Entities;
+using App.BusinessLayer.Interfaces.Repositories;
+using App.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DataLayer.Repositories
+{
+    public class SupplierRepository(MsSQLServerContext msSQLServer) : Repository<Supplier>(msSQLServer), ISupplierRepository
+    {
+        public async Task<Supplier?> GetSupllierAndAdress(Guid supplierId)
+        {
+            return await Db.Suppliers.AsNoTracking().Include(s => s.Address).FirstOrDefaultAsync(s => s.Id.Equals(supplierId));
+        }
+
+        public async Task<Supplier?> GetSupllierProductsAndAdress(Guid supplierId)
+        {
+            return await Db.Suppliers.AsNoTracking().Include(s => s.Address).Include(s => s.Products).FirstOrDefaultAsync(s => s.Id.Equals(supplierId));
+        }
+
+        public async Task<Address?> GetSupplierAddress(Guid supplierId)
+        {
+            return await Db.Addresses.AsNoTracking().FirstOrDefaultAsync(s => s.SupplierId.Equals(supplierId));
+        }
+
+        public async Task RemoveSupplierAddress(Address address)
+        {
+            Db.Addresses.Remove(address);
+            await SaveChanges();
+        }
+    }
+}
