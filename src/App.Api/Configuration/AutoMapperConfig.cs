0cc373ba1f3940a32661dac2f65abe82f1739a65
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-30 07:21:38 -0300

    feature: create modelview class and configuring automapper from and to modelview/entity

---
+++ src/App.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using App.Api.ViewModels.Response;
+using App.BusinessLayer.Entities;
+using AutoMapper;
+
+namespace App.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig() {
+            CreateMap<Supplier, SupplierViewModel>().ReverseMap();
+            CreateMap<Address, AddressViewModel>().ReverseMap();
+            CreateMap<ProductViewModel, Product>().ForMember(dest => dest.RegisterDate, opt => opt.MapFrom(src => src.RegisteredAt));
+            CreateMap<Product , ProductViewModel>().ForMember(dest => dest.SupplierName, opt => opt.MapFrom(src => src.Supplier.Name));
+        
+        }
+    }
+}
---
+++ src/App.Api/ViewModels/Response/SupplierViewModel.cs
@@ -0,0 +1,23 @@
+﻿using static App.Api.Const.DataAnnotationMessageConst;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace App.Api.ViewModels.Response
+{
+    public record SupplierViewModel
+    {
+        public Guid Id { get; init; }
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(100, ErrorMessage = StringLengthFieldMessage, MinimumLength = 2)]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(14, ErrorMessage = StringLengthFieldMessage, MinimumLength = 11)]
+        public string? Document { get; init; }
+        public int SupplierType { get; init; }
+        public bool Active { get; init; }
+        public AddressViewModel? Address { get; init; }
+        public IEnumerable<AddressViewModel>? Products { get; init; }
+
+    }
+}
