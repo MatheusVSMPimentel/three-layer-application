86c1df8586a8b401c96e24896a5d4b585f8fe085
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-30 07:35:51 -0300

    feature: main controller to deal with response and notifier object

---
+++ src/App.Api/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using App.BusinessLayer.Notifications;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
+
+namespace App.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class MainController(IMapper mapper, INotifier notifier) : ControllerBase
+    {
+        protected readonly IMapper _mapper = mapper;
+
+        protected bool ValidOperation() => !notifier.HasNotification();
+        protected ActionResult CustomResponse(HttpStatusCode statusCode = HttpStatusCode.OK, object? result = null) => ValidOperation()? new ObjectResult(result) { StatusCode = Convert.ToInt32(statusCode) } : BadRequest(new {errors = notifier.GetNotifications().Select(n => n.Message)});
+        protected ActionResult CustomResponse(ModelStateDictionary modelState ) => modelState.IsValid? CustomResponse() : ModelStateErrors(modelState);
+        protected ActionResult ModelStateErrors(ModelStateDictionary modelState ) { NotifyModelStateErrors(modelState); return CustomResponse(); }
+        protected void NotifyModelStateErrors(ModelStateDictionary modelState ) => modelState.Values.SelectMany(e => e.Errors).ToList().ForEach(e => NotifyError(e.Exception?.Message ?? e.ErrorMessage));
+        protected void NotifyError(string message) => notifier.Handle(message);
+
+
+    }
+}
