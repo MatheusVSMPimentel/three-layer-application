80860c172a520c49e1c9d1fef837069072610c15
Matheus Vinicius Silva Miranda Pimentel <matheusvsm.pimentel@hotmail.com>
2024-09-30 07:22:28 -0300

    feature: DI of services and repository

---
+++ src/App.Api/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,28 @@
+﻿using App.BusinessLayer.Interfaces.Repositories;
+using App.BusinessLayer.Interfaces.Services;
+using App.BusinessLayer.Notifications;
+using App.BusinessLayer.Services;
+using App.DataLayer.Context;
+using App.DataLayer.Repositories;
+
+namespace App.Api.Configuration
+{
+    public static class DependecyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            //Data
+            services.AddScoped<MsSQLServerContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+
+            //Business
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ISupplierService, SupplierService>();
+            services.AddScoped<INotifier, Notifier>();
+
+
+            return services;
+        }
+    }
+}
